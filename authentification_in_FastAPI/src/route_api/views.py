93d5ba75d5064ee9d28c37480cdd9d73754f623d
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-07-27 15:51:28 +0700

    add cookie

--- authentification_in_FastAPI/src/route_api/views.py
+++ authentification_in_FastAPI/src/route_api/views.py
@@ -1,6 +1,8 @@
 import secrets
-from typing import Annotated
-from fastapi import APIRouter, Depends, HTTPException, status
+import uuid
+from time import time
+from typing import Annotated, Any
+from fastapi import APIRouter, Depends, HTTPException, status, Header, Response, Cookie
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 router = APIRouter(prefix="/demo-auth", tags=["Demo auth"])
@@ -24,10 +26,15 @@ usernames_to_password = {
     "John": "password",
 }
 
+static_auth_token_to_username = {
+    "73d119cb9cffa5c262f4ac7e1c365e": "admin",
+    "085048ce5d928b5aeaa6d7355d336f5aca": "john",
+}
+
 
 def get_auth_user_username(
     credentials: Annotated[HTTPBasicCredentials, Depends(security)],
-):
+) -> str:
     unathead_exc = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Invalid username or password",
@@ -45,6 +52,17 @@ def get_auth_user_username(
     return credentials.username
 
 
+def get_username_by_static_auth_token(
+    static_token: str = Header(alias="static-auth-token"),
+) -> str:
+    if username := static_auth_token_to_username.get(static_token):
+        return username
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="token invalid",
+    )
+
+
 @router.get("/basic-auth-username/")
 def demo_basic_auth_username(
     auth_username: str = Depends(get_auth_user_username),
@@ -52,4 +70,73 @@ def demo_basic_auth_username(
     return {
         "message": f"Hi {auth_username}!",
         "username": auth_username,
+    }
+
+
+@router.get("/basic-http-header-auth/")
+def demo_auth_some_http_header(
+    username: str = Depends(get_username_by_static_auth_token),
+):
+    return {
+        "message": f"Hi {username}!",
+        "username": username,
+    }
+
+
+COOKIES: dict[str, dict[str, Any]] = {}
+COOKIES_SESSION_ID_KEY = "web-app-session-id"
+
+
+def generation_session_id() -> str:
+    return uuid.uuid4().hex
+
+
+def get_session_data(
+    session_id: str = Cookie(alias=COOKIES_SESSION_ID_KEY),
+) -> dict:
+    if session_id not in COOKIES:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="not authenticated",
+        )
+    return COOKIES[session_id]
+
+
+@router.post("/login-cookie/")
+def demo_auth_login_set_cookie(
+    response: Response,
+    # auth_username: str = Depends(get_auth_user_username),
+    username: str = Depends(get_username_by_static_auth_token),
+):
+    session_id = generation_session_id()
+    COOKIES[session_id] = {
+        "username": username,
+        "login_at": int(time()),
+    }
+    response.set_cookie(COOKIES_SESSION_ID_KEY, session_id)
+    return {"result": "OK"}
+
+
+@router.get("/check-cookie/")
+def demo_auth_check_cookie(
+    user_session_data: dict = Depends(get_session_data),
+):
+    username = user_session_data["username"]
+    return {
+        "Message": f"Hello, {username}!",
+        **user_session_data,
+    }
+
+
+@router.get("/logout-cookie/")
+def demo_auth_logout_cookie(
+    response: Response,
+    session_id: str = Cookie(alias=COOKIES_SESSION_ID_KEY),
+    user_session_data: dict = Depends(get_session_data),
+):
+    COOKIES.pop(session_id)
+    response.delete_cookie(COOKIES_SESSION_ID_KEY)
+    username = user_session_data["username"]
+    return {
+        "Message": f"Bye, {username}!",
     }
